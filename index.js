2a40c114cdc10858eb5f64588058a8e0761361a6
oladapo <oomage485@gmail.com>
2026-01-01 12:19:53 -0800

    dfgh

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-window.addEventListener("scroll", () => {
-    const navbar = document.querySelector(".navbar");
-    navbar.classList.toggle("scrolled", window.scrollY > 50);
-});
-
 
 // gsap section
 gsap.registerPlugin(ScrollTrigger);
